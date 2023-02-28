175087093743ef5b47995bb1028e89659538d659
markmuhari <markmuhari.eu@gmail.com>
2023-02-28 19:32:08 +0100

    WebSecurityConfigurerAdapter not working. servlet.Filter   class file for javax.servlet.Filter not found

---
+++ src/main/java/hu/markmuhari/login_reg_and_email_verification_backend/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package hu.markmuhari.login_reg_and_email_verification_backend.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public static String register(RegistrationRequest request) {
+        return "works";
+    }
+}
---
+++ src/main/java/hu/markmuhari/login_reg_and_email_verification_backend/security/PasswordEncoder.java
@@ -0,0 +1,15 @@
+package hu.markmuhari.login_reg_and_email_verification_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
