b2c48e0cc45febcaf1b15e3727402a2dd7d13afd
markmuhari <markmuhari.eu@gmail.com>
2023-03-01 23:56:42 +0100

    added MailDev and expires

---
+++ src/main/java/hu/markmuhari/login_reg_and_email_verification_backend/email/EmailSender.java
@@ -0,0 +1,6 @@
+package hu.markmuhari.login_reg_and_email_verification_backend.email;
+
+public interface EmailSender {
+
+    void send(String to, String email);
+}
---
+++ src/main/java/hu/markmuhari/login_reg_and_email_verification_backend/email/EmailService.java
@@ -0,0 +1,43 @@
+package hu.markmuhari.login_reg_and_email_verification_backend.email;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private final static String FAILED_TO_SEND_MSG ="Failed to send %s email";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email.");
+            helper.setFrom("hello@markmuhari.com");
+
+            mailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            LOGGER.error(String.format(FAILED_TO_SEND_MSG, email), e);
+            throw new IllegalStateException(FAILED_TO_SEND_MSG);
+        }
+    }
+}
